256fa7ba39325290beb0bcbbfed80413c8671040
naveennaidu <naveen@zeitalabs.com>
2025-07-23 10:45:49 +0530

    Initial Commit

---
+++ ccleaderboard/ccleaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ccleaderboardApp.swift
+//  ccleaderboard
+//
+//  Created by Naveennaidu Mummana on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ccleaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
